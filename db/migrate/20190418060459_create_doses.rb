772e3b2ca8b5027fd7dd67a9838b7b63ea55d8d8
Dave Watson <watson_da@hotmail.co.uk>
2019-04-18 15:14:26 +0800

    updated validations

---
+++ db/migrate/20190418060459_create_doses.rb
@@ -0,0 +1,11 @@
+class CreateDoses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :doses do |t|
+      t.string :name
+      t.string :description
+      t.references :ingredient, foreign_key: true
+      t.references :cocktail, foreign_key: true
+      t.timestamps null: false
+    end
+  end
+end
