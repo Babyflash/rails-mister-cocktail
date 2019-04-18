3e048fcaa88322e5371839264074335de4e6d8da
Dave Watson <watson_da@hotmail.co.uk>
2019-04-18 16:29:16 +0800

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_serialized = open(url).read
+ingredient_list = JSON.parse(ingredient_serialized)
+ingredient = ingredient_list['drinks']
+
+ingredient.each do |ing|
+  Ingredient.create(name: ing['strIngredient1'])
+end
+
